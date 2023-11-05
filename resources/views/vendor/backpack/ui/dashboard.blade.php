0205374718c2e54738851ceae099c497ed3af4ac
Matt Dunbar <matt@mattdsworld.com>
2023-11-05 16:17:13 +0700

    Fix dashboard, add admin auth

---
+++ resources/views/vendor/backpack/ui/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends(backpack_view('blank'))
+
+@php
+    if (backpack_theme_config('show_getting_started')) {
+        $widgets['before_content'][] = [
+            'type'        => 'view',
+            'view'        => backpack_view('inc.getting_started'),
+        ];
+    }
+@endphp
+
+@section('content')
+@endsection
