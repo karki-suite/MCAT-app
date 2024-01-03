92144e9b7ee9653af86744ba4e06ad3930c9c0f5
karki-suite <147542087+karki-suite@users.noreply.github.com>
2024-01-02 23:21:28 -0600

    Tried to fix layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,7 +106,8 @@
                                 <div class="counter_item_h">
                                     <div class="title">
                                         <h2 class="counter text-gradient">{{ $content['home-feature-' . ($i+1) . '-number'] }}</h2>
-										<h3 class="text-gradient">{{ $content['home-feature-' . ($i+1) . '-symbol'] ?? ' ' }}</h3>                                    </div>
+										<h3 class="text-gradient">{!! $content['home-feature-' . ($i+1) . '-symbol'] ?? ' ' !!}</h3>                                    
+									</div>
                                     <h5>{{ $content['home-feature-' . ($i+1) . '-title'] }}</h5>
 									<p class="text-sm">{!! $content['home-feature-' . ($i+1) . '-summary'] !!}</p>
                                     <hr class="vertical dark">
